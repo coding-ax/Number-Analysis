69a1eb00bfd8f684cdacd0133632b56a4e8d5ed9
whutfaker <1312976124@qq.com>
2020-04-29 00:00:47 +0800

    拉格朗日插值法

--- 实验一/Lagrange.py
+++ 实验一/Lagrange.py
@@ -1,14 +1,32 @@
-x = float(input("x="))  # 输入x
-n = int(input("n="))  # 输入插入节点个数n
-fxNum = []
-# 循环输入xi与yi
-for value in range(0, n):
-    xi = float(input('x' + str(value) + ':'))
-    yi = float(input('y' + str(value) + ':'))
-    fxNum.append({
-        'x': xi,
-        'y': yi
-    })
+# x = float(input("x="))  # 输入x
+# n = int(input("n="))  # 输入插入节点个数n
+# fxNum = []
+# # 循环输入xi与yi
+# for value in range(0, n):
+#     xi = float(input('x' + str(value) + ':'))
+#     yi = float(input('y' + str(value) + ':'))
+#     fxNum.append({
+#         'x': xi,
+#         'y': yi
+#     })
+
+x = 115.0
+n = 3
+fxNum = [
+    {
+        'x': 100.0,
+        'y': 10.0
+    },
+    {
+        'x': 121.0,
+        'y': 11.0
+    },
+    {
+        'x': 144.0,
+        'y': 12.0
+    }
+]
+
 k = 0
 y = 0
 while k != n:
